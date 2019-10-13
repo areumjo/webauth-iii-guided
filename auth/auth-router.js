c332dbd48eb4f6291815901a2915b1f8295115a5
Areum Jo <areumjo1@gmail.com>
2019-10-13 11:50:52 -0700

    Added token to restricted-middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const jwt = requrie('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -13,7 +13,11 @@ router.post('/register', (req, res) => {
   Users.add(user)
     .then(saved => {
       // a jwt should be generated
-      res.status(201).json(saved);
+      const token = generateToken(saved)
+      res.status(201).json({
+        user: saved,
+        token
+      });
     })
     .catch(error => {
       res.status(500).json(error);
@@ -27,8 +31,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // a jwt should be generated
+        const token = generateToken(user);
+        
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -48,7 +56,7 @@ function generateToken(user) {
   };
   const options = {
     expiresIn: '1d'
-  }
+  } // 1d (1day), 100ms (it will give you... name: 'TokenExpiredError', message: 'jwt expired')
   // // v signature -> a secret
 
 
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,25 +1,54 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
   // we don't wanna check username/password ==> need to refactor
-  const { username, password } = req.headers;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  const token = req.headers.authorization;
+
+  // see if there is a token
+  // check if it's valid =>
+  //    rehash the header + payload + secret and see if matches our verify signature
+  
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log('failed verify ,', err);
+        res.status(401).json({
+          message: 'not veified'
+        })
+      } else {
+        // token is valid
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({
+      message: 'no token provided'
+    })
   }
+
+
+  // const { username, password } = req.headers;
+
+  // if (username && password) {
+  //   Users.findBy({ username })
+  //     .first()
+  //     .then(user => {
+  //       if (user && bcrypt.compareSync(password, user.password)) {
+  //         next();
+  //       } else {
+  //         res.status(401).json({ message: 'Invalid Credentials' });
+  //       }
+  //     })
+  //     .catch(error => {
+  //       res.status(500).json({ message: 'Ran into an unexpected error' });
+  //     });
+  // } else {
+  //   res.status(400).json({ message: 'No credentials provided' });
+  // }
+  
 };
